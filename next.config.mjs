5da292245d352d6faf5e637c1ddf74e1e4e1e1a9
d-ravi-kumar19 <dravikumar19@outlook.com>
2024-12-02 16:39:49 +0530

    configuration changed for deploy

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    basePath: "/photography-website",  
+    assetPrefix: "/photography-website/", 
+    output: 'export',  // Makes it static export for GitHub Pages
+    images: {
+      loader: "imgix", // Uses imgix loader for optimized image loading in a static site
+      path: "",  
+    },
+  };
+  
+  export default nextConfig;
+  
