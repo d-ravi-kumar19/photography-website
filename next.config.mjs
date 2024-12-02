0b251df9951ae2b1eb694d8ea9bc3929a0d851f3
d-ravi-kumar19 <dravikumar19@outlook.com>
2024-12-02 17:26:17 +0530

    updated

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    basePath: "/photography-website",  
-    assetPrefix: "/photography-website/", 
-    output: 'export',  // Makes it static export for GitHub Pages
-    images: {
-      loader: "imgix", // Uses imgix loader for optimized image loading in a static site
-      path: "",  
-    },
-  };
-  
-  export default nextConfig;
-  
+  basePath: "/photography-website", // Base path for the site (useful for deployment on subdirectories)
+  assetPrefix: "/photography-website/", // Prefix for static assets
+  output: "export", // Enables static export (for GitHub Pages or similar)
+  images: {
+    loader: "imgix", // Uses imgix loader for handling images
+    path: "", // Required for static exports; ensures images load correctly
+    unoptimized: true, // Prevents automatic image optimization, needed for static export
+  },
+  eslint: {
+    ignoreDuringBuilds: true, // Ignores ESLint errors during the build process
+  },
+};
+
+export default nextConfig;
